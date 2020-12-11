8d9b516d221eb911dfbc8a1734273452f0657039
beahoop <beahoopdesign@gmail.com>
2020-12-11 13:12:44 -0500

    Update
    Updated

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
+function productdetails() {
+  var x = document.getElementById("myproductdetails");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
