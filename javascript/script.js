c36ad3d2d9e5ea73e72ff35b51f234402e8193fe
David Paul <david.paul@juiceanalytics.com>
2020-12-11 12:30:23 -0600

    added new js function

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,21 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
+const productDetails = () => {
+  let x = document.getElementById("productDetails");
+  let y = document.getElementById("sizeAndFit")
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none"
   } else {
     x.style.display = "none";
   }
-}
+};
+
+const sizeAndFit = () => {
+  let x = document.getElementById("sizeAndFit");
+  let y = document.getElementById("productDetails")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+};
