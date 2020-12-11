83d4d26e236c699b1887f667b18d9898754bf3ca
beahoop <beahoopdesign@gmail.com>
2020-12-11 13:24:16 -0500

    Update
    Update for david

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,24 @@
 function productdetails() {
   var x = document.getElementById("myproductdetails");
+    myreview.style.visibility = 'hidden';
+  if (x.style.display === "none") {
+    x.style.display = "block";
+
+  } else {
+    x.style.display = "none";
+  }
+}
+function sizefit() {
+   myreview.style.visibility = 'hidden';
+  var x = document.getElementById("mysizefit");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function review() {
+  var x = document.getElementById("myreview");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
